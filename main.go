74f045144e68580265801db128cdd9df098059df
Molly Miller <sysvinit@users.noreply.github.com>
2021-03-07 22:01:13 +0000

    Add ability to specify configuration file directly on command line.
    This is useful if you want to run multiple instances of irccat simultaneously.

--- main.go
+++ main.go
@@ -35,10 +35,16 @@ func main() {
 
 	loggo.ConfigureLoggers("<root>=INFO")
 	log.Infof("IRCCat %s (%s) starting...", branch, revision)
-	viper.SetConfigName("irccat")
-	viper.AddConfigPath("/run/secrets")
-	viper.AddConfigPath("/etc")
-	viper.AddConfigPath(".")
+
+	if len(flag.Args()) > 0 {
+		viper.SetConfigFile(flag.Args()[0])
+	} else {
+		viper.SetConfigName("irccat")
+		viper.AddConfigPath("/run/secrets")
+		viper.AddConfigPath("/etc")
+		viper.AddConfigPath(".")
+	}
+
 	var err error
 
 	err = viper.ReadInConfig()
